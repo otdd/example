299bc38a582854696a8f0e48c7bae0b4795143f2
yipjie <dev@otdd.io>
2020-02-16 12:46:04 +0800

    update

---
+++ dependent-mysql/schema.sql
@@ -0,0 +1,7 @@
+use hello_otdd;
+CREATE TABLE hello_otdd_tb
+(
+id INTEGER AUTO_INCREMENT,
+value VARCHAR(200),
+PRIMARY KEY (id)
+) COMMENT='this is hello otdd table';
--- service-http/service-http.go
+++ service-http/service-http.go
@@ -12,6 +12,9 @@ import (
 	pb "otdd.io/example/dependent-grpc/grpc"
 	"github.com/apache/thrift/lib/go/thrift"
 	service "otdd.io/example/dependent-thrift/service"
+	"github.com/go-redis/redis"
+	"database/sql"
+	_"github.com/go-sql-driver/mysql"
 )
  
 func SayHello(w http.ResponseWriter, r *http.Request) {
@@ -68,6 +71,65 @@ func SayHello(w http.ResponseWriter, r *http.Request) {
 			}
 		}
 	}
+	// call redis dependent
+	client := redis.NewClient(&redis.Options{ 
+		Addr: "dependent-redis:6379",
+	})
+	err = client.Set("key", "Hello OTDD", 0).Err()
+	if err != nil {
+		io.WriteString(w, "Failed to set key to dependent-redis:"+err.Error()+"\n")
+	} else {
+		val, err := client.Get("key").Result() 
+		if err != nil { 
+			io.WriteString(w, "Failed to get value from dependent-redis:"+err.Error()+"\n")
+		} else {
+			io.WriteString(w, "value from dependent-redis:"+val+"\n")
+		}
+	}
+
+	// call mysql dependent
+	db, err := sql.Open("mysql","root:123456@tcp(dependent-mysql:3306)/hello_otdd")
+	if err != nil {
+		io.WriteString(w, "Failed to connect to dependent-mysql:"+err.Error()+"\n")
+	} else {
+		defer db.Close()
+		stmt, err := db.Prepare("insert into hello_otdd_tb(value) VALUES(?)")
+		if err != nil {
+			io.WriteString(w, "failed to prepare insert statement:"+err.Error()+"\n")
+		}
+		res, err := stmt.Exec("Hello OTDD")
+		if err != nil {
+			io.WriteString(w, "failed to execute insert statement:"+err.Error()+"\n")
+		} else {
+			id, _ := res.LastInsertId()
+			io.WriteString(w, fmt.Sprintf("inserted row to table, id:%d \n",id))
+			rows, err := db.Query("select * from hello_otdd_tb order by id desc limit 1")
+			if err != nil {
+				io.WriteString(w, "Failed to select from dependent-mysql:"+err.Error()+"\n")
+			} else {
+				//https://kylewbanks.com/blog/query-result-to-map-in-golang
+				cols, _ := rows.Columns()
+				for rows.Next() {
+					columns := make([]interface{}, len(cols))
+					columnPointers := make([]interface{}, len(cols))
+					for i, _ := range columns {
+						columnPointers[i] = &columns[i]
+					}
+					if err := rows.Scan(columnPointers...); err != nil {
+						io.WriteString(w, fmt.Sprintf("failed to parse row from dependent-mysql:%v \n", rows))
+					} else {
+						 m := make(map[string]interface{})
+						for i, colName := range cols {
+							val := columnPointers[i].(*interface{})
+							m[colName] = *val
+						}
+						io.WriteString(w, fmt.Sprintf("row with max id from dependent-mysql:%v \n", m))
+					}
+				}
+			}
+		}
+	}
+	
 }
 
 // formatRequest generates ascii representation of a request
